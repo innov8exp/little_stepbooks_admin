a17e97c937aeaab39fd2db63b6249380092a08c9
spring_falling <spring_falling@163.com>
2024-06-20 09:20:04 +0800

    移除活动入口

--- src/router/activity.jsx
+++ src/router/activity.jsx
@@ -1,23 +1,23 @@
 import Advertisement from '@/pages/advertisement'
-import Activity from '@/pages/activity'
-import ActivityAudioList from '@/pages/activity/audioList'
 import DailyAudioListPage from '@/pages/activity/dailyAudioList'
+// import Activity from '@/pages/activity'
+// import ActivityAudioList from '@/pages/activity/audioList'
 
 const routes = [
     { path: '/advertisement-list', element: <Advertisement />, label: 'menu.advertisement', isMenu: true },
-    { path: '/activity-list', element: <Activity />, label: 'menu.activityList', isMenu: true },
-    {
-        path: '/activity/:activityId/audios',
-        element: <ActivityAudioList />,
-        label: 'button.mediaManage',
-        parentPath: '/activity-list'
-    },
     {
         path: '/daily-audios-list',
         element: <DailyAudioListPage />,
         label: 'dailyAudio',
         isMenu: true
     }
+    // { path: '/activity-list', element: <Activity />, label: 'menu.activityList', isMenu: true },
+    // {
+    //     path: '/activity/:activityId/audios',
+    //     element: <ActivityAudioList />,
+    //     label: 'button.mediaManage',
+    //     parentPath: '/activity-list'
+    // },
 ]
 
 export default routes
